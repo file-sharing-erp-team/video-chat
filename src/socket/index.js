efab71418feb5c9bb9a6af13de7448d7dc253aef
sigarachi <lalalabtv@gmail.com>
2022-06-15 09:47:24 +0300

    123

--- src/socket/index.js
+++ src/socket/index.js
@@ -7,6 +7,6 @@ const options = {
     transports: ["websocket"]
 }
 
-const socket = io('http://localhost:3001', options);
+const socket = io('http://university-ecosystem.ru/video-api', options);
 
 export default socket;
